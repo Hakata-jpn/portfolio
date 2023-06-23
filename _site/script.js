3a8201916c361eb1d1477f7371d09b99a2545e9d
Hakata-jpn <blandingren425@gmail.com>
2023-06-23 14:37:39 -0500

    fixed mobile header script, needs some hover and active states

--- _site/script.js
+++ _site/script.js
@@ -1,17 +1,16 @@
 let navbar = document.getElementById("navbar-toggle");
-let button = document.getElementById("hamburger-menu")
+let button = document.getElementById("hamburger-menu");
 
-function toggleDiv() {
-  if (navbar.style.display === "block") {
-    navbar.style.display = "none";
-  } else {
-    navbar.style.display = "block";
-  };
-}
-
-function Collapse() {
-  let buttonVisible = getComputedStyle(button);
-  if (buttonVisible.display == "block") {
-    toggleDiv();
-  }
+if (screen.width <= 768) {
+    button.addEventListener("click", function() {
+        navbar.classList.toggle("visible");
+        navbar.classList.toggle("invisible");
+      });
+      
+      navbar.addEventListener("click", function() {
+          navbar.classList.toggle("visible");
+          navbar.classList.toggle("invisible");
+      });
+} else {
+    navbar.classList.remove("invisible")
 }
